f489a2f5f43ae4d744848d3f6137b455a03645bb
Aayush36049 <ayush73215@gmail.com>
2025-09-18 14:57:18 +0530

    Add files via upload

---
+++ 6.2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swapvalue(int a, int b)   {
+    int temp;
+     temp = a;
+     a = b;
+     b = temp;
+     printf("inside swapvalue - a: %d, b: %d\n" ,  a, b);
+
+}
+int main () {
+    int a, b;
+
+    printf("enter two numbers: ");
+    scanf("%d %d",  &a, &b);
+
+    printf("before swapvalue - a: %d, b: %d\n", a, b);
+    swapvalue(a, b);
+    printf("after swapvalue - a: d, b: %d\n", a, b);
+
+    return 0;
+}
